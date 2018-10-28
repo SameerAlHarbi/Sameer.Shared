56a9fc5164275902c7967b5a8860cace2da8f041
Sameer Al Harbi <smrhrbi@gmail.com>
2018-10-28 22:56:11 +0300

    Get Info

--- Sameer.Shared/Sameer.Shared.Helpers/DateHelpers.cs
+++ Sameer.Shared/Sameer.Shared.Helpers/DateHelpers.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 
 namespace Sameer.Shared.Helpers
@@ -7,38 +6,9 @@ namespace Sameer.Shared.Helpers
     public static class DateHelpers
     {
 
-        public static string ConvertToString(this DateTime date, bool hijri = false
-            , bool showDayName = false, bool showDayNameInArabic = false
-            , string format = "yyyy/MM/dd")
-        {
-            if(showDayName)
-            {
-                return  $"{date.GetDayName(showDayNameInArabic)} {date.ConvertToString(hijri, format)}";
-            }
-
-            return date.ConvertToString(hijri,format);
-        }
-
-        public static string ConvertToString(this DateTime date,bool hijri = false
-            , string format = "yyyy/MM/dd")
-        {
-            CultureInfo culture = new CultureInfo(hijri? "ar-SA":"en-US");
-
-            if(hijri)
-            {
-                culture.DateTimeFormat.Calendar = new UmAlQuraCalendar();
-            }
-            else
-            {
-                culture.DateTimeFormat.Calendar = new GregorianCalendar(GregorianCalendarTypes.USEnglish);
-            }
-            
-            return date.ToString(format, culture);
-        }
-
         public static string GetDayName(this DateTime date, bool showDayNameInArabic = false)
         {
-            if(!showDayNameInArabic)
+            if (!showDayNameInArabic)
             {
                 return date.DayOfWeek.ToString();
             }
@@ -56,10 +26,35 @@ namespace Sameer.Shared.Helpers
                 case DayOfWeek.Thursday:
                     return "الخميس";
                 case DayOfWeek.Friday:
-                    return "الجمعة";           
+                    return "الجمعة";
                 default:
                     return "السبت";
             }
+        }
+
+        public static string ConvertToString(this DateTime date, bool hijri = false
+            , bool showDayName = false, bool showDayNameInArabic = false
+            , string format = "yyyy/MM/dd")
+        {
+            CultureInfo culture = new CultureInfo(hijri ? "ar-SA" : "en-US");
+
+            if (hijri)
+            {
+                culture.DateTimeFormat.Calendar = new UmAlQuraCalendar();
+            }
+            else
+            {
+                culture.DateTimeFormat.Calendar = new GregorianCalendar(GregorianCalendarTypes.USEnglish);
+            }
+
+            var resultDateString = date.ToString(format, culture);
+
+            if (showDayName)
+            {
+                return  $"{date.GetDayName(showDayNameInArabic)} {resultDateString}";
+            }
+
+            return resultDateString;
         }
 
         public static DateTime ConvertToDate(this string dateString, bool hijriDateString = false, string format = "yyyy/MM/dd")
@@ -71,12 +66,77 @@ namespace Sameer.Shared.Helpers
             return resultDate;
         }
 
-        public static DateTime ConvertToGregorianDate([Range(1300,1500,ErrorMessage = "Invalid hijri year !")]int hijriYear, [Range(1, 12, ErrorMessage = "Invalid hijri month !")]int hijriMonth, [Range(1,30, ErrorMessage = "Invalid hijri day !")]int hijriDay)
+        public static DateTime ConvertToGregorianDate(int hijriYear,int hijriMonth,int hijriDay)
         {
+            if ((hijriDay < 1) || (hijriDay > 30))
+            {
+                throw new Exception("Invalid hijri day value not between 1 and 30");
+            }
+            if ((hijriMonth < 1) || (hijriMonth > 12))
+            {
+                throw new Exception("Invalid hijri month value not between 1 and 12");
+            }
+            if ((hijriYear < 1300) || (hijriYear > 2000))
+            {
+                throw new Exception("Invalid hijri year value");
+            }
             CultureInfo arCul = new CultureInfo("ar-SA");
             arCul.DateTimeFormat.Calendar = new UmAlQuraCalendar();
             string dateString = string.Format("{0:0000}/{1:00}/{2:00}", hijriYear, hijriMonth, hijriDay);
             return dateString.ConvertToDate(true);
+        }
+
+        public static int GetYear(this DateTime date,bool hijri=false)
+        {
+            CultureInfo culture = new CultureInfo(hijri? "ar-SA" : "en-US");
+
+            if (hijri)
+            {
+                culture.DateTimeFormat.Calendar = new UmAlQuraCalendar();
+            }
+            else
+            {
+                culture.DateTimeFormat.Calendar = new GregorianCalendar(GregorianCalendarTypes.USEnglish);
+            }
+
+            return int.Parse(date.ToString("yyyy", culture));
+        }
+
+        public static int GetMonth(this DateTime date, bool hijri = false)
+        {
+            CultureInfo culture = new CultureInfo(hijri? "ar-SA" : "en-US");
+
+            if (hijri)
+            {
+                culture.DateTimeFormat.Calendar = new UmAlQuraCalendar();
+            }
+            else
+            {
+                culture.DateTimeFormat.Calendar = new GregorianCalendar(GregorianCalendarTypes.USEnglish);
+            }
+
+            return int.Parse(date.ToString("MM", culture));
+        }
+
+        public static int GetDay(this DateTime date, bool hijri = false)
+        {
+            CultureInfo culture = new CultureInfo(hijri ? "ar-SA" : "en-US");
+
+            if (hijri)
+            {
+                culture.DateTimeFormat.Calendar = new UmAlQuraCalendar();
+            }
+            else
+            {
+                culture.DateTimeFormat.Calendar = new GregorianCalendar(GregorianCalendarTypes.USEnglish);
+            }
+
+            return int.Parse(date.ToString("dd", culture));
+        }
+
+        public static (int year, int month ,int day) GetInfo(this DateTime date, bool hijri = false)
+        {
+
         }
     }
 }
