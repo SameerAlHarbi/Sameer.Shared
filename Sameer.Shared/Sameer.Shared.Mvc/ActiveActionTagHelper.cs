3341b4a6c33e0f6df3297fa60f91b501f23b8622
Sameer Al Harbi <smrhrbi@gmail.com>
2019-02-05 03:48:30 +0300

    update shared mvc

---
+++ Sameer.Shared/Sameer.Shared.Mvc/ActiveActionTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+
+namespace Sameer.Shared.Mvc
+{
+    [HtmlTargetElement(Attributes = "is-active-action")]
+    public class ActiveActionTagHelper: ActiveTagHelper
+    {
+        public ActiveActionTagHelper()
+        {
+            this.tagHelperName = "is-active-action";
+        }
+        protected override bool ShouldBeActive()
+        {
+            string currentAction = ViewContext.RouteData.Values["Action"].ToString();
+
+            if (!string.IsNullOrWhiteSpace(Action) && Action.ToLower() != currentAction.ToLower())
+            {
+                return false;
+            }
+
+            foreach (KeyValuePair<string, string> routeValue in RouteValues)
+            {
+                if (!ViewContext.RouteData.Values.ContainsKey(routeValue.Key) ||
+                    ViewContext.RouteData.Values[routeValue.Key].ToString() != routeValue.Value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Sameer.Shared/Sameer.Shared.Mvc/ActiveRouteTagHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+
+namespace Sameer.Shared.Mvc
+{
+    [HtmlTargetElement(Attributes = "is-active-route")]
+    public class ActiveRouteTagHelper:ActiveTagHelper
+    {
+        public ActiveRouteTagHelper()
+        {
+            this.tagHelperName = "is-active-route";
+        }
+
+        protected override bool ShouldBeActive()
+        {
+            string currentController = ViewContext.RouteData.Values["Controller"].ToString();
+            string currentAction = ViewContext.RouteData.Values["Action"].ToString();
+
+            if (!string.IsNullOrWhiteSpace(Controller) && Controller.ToLower() != currentController.ToLower())
+            {
+                return false;
+            }
+
+            if (!string.IsNullOrWhiteSpace(Action) && Action.ToLower() != currentAction.ToLower())
+            {
+                return false;
+            }
+
+            foreach (KeyValuePair<string, string> routeValue in RouteValues)
+            {
+                if (!ViewContext.RouteData.Values.ContainsKey(routeValue.Key) ||
+                    ViewContext.RouteData.Values[routeValue.Key].ToString() != routeValue.Value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
